6375b98ff66042134e1b6884f3e7738fc5f87e6b
loic <loic@isima.fr>
2017-11-06 23:16:08 +0100

    Initial commit

---
+++ master.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "master.h"
+
+// un petit commentaire ?
+void initialiser_solution() {
+   // TO DO
+}
+
+// un petit commentaire ?
+void initialiser_plateau(){
+   // TO DO
+}
+
+/*
+ compiler_proposition() {
+	
+} */
---
+++ master.h
@@ -0,0 +1,22 @@
+#ifndef master_h
+#define master_h
+
+/* DECLARATION DES CONSTANTES SYMBOLIQUES */
+
+#define NB_COLONNES 4
+
+/* DECLARATION DES TYPES PERSONNELS */
+// et de leur typedef si besoin
+
+typedef struc {
+
+} combinaison;
+
+/* DECLARATIONS DES METHODES */
+
+void initialiser_solution();
+void initialiser_plateau ();
+// void compiler_proposition();
+// mettre ici les autres declarations
+
+#endif
